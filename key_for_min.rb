5868c9e71a52fb18f5927b88e63d460c25ab0236
Matt <ehxtwo@gmail.com>
2021-02-27 11:47:56 -0800

    commit tree e97dc0cf817132246ae8f595d360f0536a1bb66f

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,38 @@
+require 'pry'
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
+#create an array out of all the keys and values
+#find the place among the values of the lowest number
+#return the string preceding that place
 
+
+def key_for_min_value(name_hash)
+    lowest_key = nil
+    lowest_value = 0
+    name_hash.each do |key, value|
+        if lowest_value > value || lowest_value == 0
+            lowest_value = value 
+            lowest_key = key
+        end
+    # if the lowest value is less than the value
+    # or if lowest value equals zero
+    end
+     return lowest_key
 end
+
+
+
+
+
+ # array = []
+    # if name_hash == {}
+    #     return nil
+    # else 
+       
+    #     name_hash.each do |object, value|
+    #        array = array.push "#{object}", "#{value}"
+    #         return array
+    #     #     # array.find do {|lowest| value}
+    #     #     #     return [lowest-1]
+    #     #      end 
